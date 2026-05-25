c2b059a0980829c73a4f645edbdf61dc9e8f4dc7
TwilightFox <3150399699@qq.com>
2026-05-25 23:47:25 +0800

    Initial repository setup

---
+++ common/src/main/java/top/huliawsl/slateui/Constants.java
@@ -0,0 +1,11 @@
+package top.huliawsl.slateui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "slateui";
+    public static final String MOD_NAME = "SlateUI";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
